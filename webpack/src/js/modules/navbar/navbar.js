f4877129f27670a3579ae9f74563f0b1a438f1b1
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-05-02 22:08:49 +0200

    Search

---
+++ webpack/src/js/modules/navbar/navbar.js
@@ -0,0 +1,43 @@
+import $ from 'jquery';
+
+const stickyOn = ({target}) => {
+    const navbar = target.closest('.navbar');
+    $(navbar).addClass('sticky');
+};
+
+const stickyOff = ({target}) => {
+    const navbar = target.closest('.navbar');
+
+    if (window.pageYOffset <= 1) {
+        if (!$(navbar).find('.navbar-collapse').hasClass('show')) {
+            $(navbar).removeClass('sticky');
+        }
+    }
+};
+
+const scrollStickyTop = () => {
+    const navbar = $('.navbar');
+    if (window.pageYOffset >= 1) {
+        navbar.addClass('sticky');
+    } else {
+        if (!navbar.find('.navbar-collapse').hasClass('show')) {
+            navbar.removeClass('sticky');
+            navbar.find('.dropdown > ul').removeClass('show');
+        }
+    }
+};
+
+const scrollListener = () => {
+    const topNavbar = $('.navbar');
+
+    window.onscroll = () => scrollStickyTop();
+
+    topNavbar.on({
+        'show.bs.dropdown': (event) => stickyOn(event),
+        'hide.bs.dropdown': (event) => stickyOff(event),
+        'show.bs.collapse': (event) => stickyOn(event),
+        'hide.bs.collapse': (event) => stickyOff(event)
+    });
+};
+
+window.addEventListener('scroll', scrollListener);
